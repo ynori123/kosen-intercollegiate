5cfa6fc8dcd9dd79cf004b4012555aeff6926d0a
ynori123 <norisama1159@gmail.com>
2023-11-22 21:13:33 +0900

    ヘッダーデザインの変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,15 +11,15 @@ export default function Header() {
         <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <Icon className='w-10 h-10' />
         </Link>
-        <Link href="/" className='ml-3 text-xl'>船舶就活</Link>
+        <Link href="/" className='ml-3 text-xl hover:text-gray-900'>船舶就活</Link>
         
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center'>
-          <Link href="/event" className="mr-5 hover:text-gray-900">イベント一覧</Link>
-          <Link href="/company" className="mr-5 hover:text-gray-900">会社一覧</Link>
+          <Link href="/event" className="mr-5 hover:text-gray-900 hover:font-medium">イベント一覧</Link>
+          <Link href="/company" className="mr-5 hover:text-gray-900 hover:font-medium">会社一覧</Link>
         </nav>
         <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
-          <Link href="/login" className="mr-5 hover:text-gray-900">ログイン</Link>
-          <Link href="/signup" className="mr-5 hover:text-gray-900">登録</Link>
+          <Link href="/login" className="mr-5 hover:text-gray-900 hover:font-medium">ログイン</Link>
+          <Link href="/signup" className="mr-5 hover:text-gray-900 hover:font-medium">登録</Link>
         </nav>
       </div>
     </header>
