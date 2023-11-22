7b124da9ad80fd6c35ef17112bac2ea61197b814
ynori123 <norisama1159@gmail.com>
2023-11-22 22:21:27 +0900

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,42 +14,6 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-    fontFamily: {
-      'body': [
-      'Inter', 
-      'ui-sans-serif', 
-      'system-ui', 
-      '-apple-system', 
-      'system-ui', 
-      'Segoe UI', 
-      'Roboto', 
-      'Helvetica Neue', 
-      'Arial', 
-      'Noto Sans', 
-      'sans-serif', 
-      'Apple Color Emoji', 
-      'Segoe UI Emoji', 
-      'Segoe UI Symbol', 
-      'Noto Color Emoji'
-    ],
-      'sans': [
-        'Inter', 
-        'ui-sans-serif', 
-        'system-ui', 
-        '-apple-system', 
-        'system-ui', 
-        'Segoe UI', 
-        'Roboto', 
-        'Helvetica Neue', 
-        'Arial', 
-        'Noto Sans', 
-        'sans-serif', 
-        'Apple Color Emoji', 
-        'Segoe UI Emoji', 
-        'Segoe UI Symbol', 
-        'Noto Color Emoji'
-      ]
-    }
   },
   plugins: [],
   
