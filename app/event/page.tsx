5ae5c139f451eb1ec83036afaeee041323ad5ed0
ynori123 <norisama1159@gmail.com>
2023-11-22 23:26:34 +0900

    change design

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React, { useEffect, useRef, useState } from 'react'
 
 export default function page() {
-  const [offerItems, setofferItems] = useState<{ id: string; title: string; tag: string; summary: string; imgSrc: string; }[]>([]);
+  const [offerItems, setofferItems] = useState<{ id: string; title: string; place: string; datetime: string; imgSrc: string; }[]>([]);
   const effectRan = useRef(false)
   useEffect(() => {
     if (effectRan.current === false){
@@ -12,7 +12,7 @@ export default function page() {
           const response = await fetch('http://localhost:8000/offers');
           const data = await response.json();
           
-          const offers: [{id: string, title: string, tag: string, summary: string, imgSrc: string}] = data?.data?.offers || [];
+          const offers: [{id: string, title: string, place: string, datetime: string, imgSrc: string}] = data?.data?.offers || [];
           
           setofferItems(offers);
         } catch (error) {
@@ -27,17 +27,17 @@ export default function page() {
     }
 
   }, []);
-  const OfferCard = (item: { id: string; title: string; tag: string; summary: string; imgSrc: string; }) => (
+  const OfferCard = (item: { id: string; title: string; place: string; datetime: string; imgSrc: string; }) => (
     <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
       <Link href={`/event/${item.id}`}>
         <div className='m-1 p-8 bg-zinc-200 rounded-lg hover:bg-zinc-300'>
         <a className="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={item.imgSrc} />
+          <img alt="求人イベント" className="object-cover object-center w-full h-full block" src={item.imgSrc} />
         </a>
         <div className="mt-4">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.tag}</h3>
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.place}</h3>
           <h2 className="text-gray-900 title-font text-lg font-medium">{item.title}</h2>
-          <p className="mt-1">{item.summary}</p>
+          <p className="mt-1">{item.datetime}</p>
         </div>
         </div>
       </Link>
