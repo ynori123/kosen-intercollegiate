ef649d95d1f03a779a053db2139b1e9b99d962ea
se4weed <norisama1159@gmail.com>
2023-12-07 02:38:50 +0900

    テストケースを修正

--- test/testserver/data/user.py
+++ test/testserver/data/user.py
@@ -9,14 +9,14 @@ users = [
     {
         "id" : "2",
         "name" : "test2",
-        "email" : "testuser5678@kosen.com",
+        "email" : "testuser2345@kosen.com",
         "password" : "testuser2345",
         "token" : "4a879bb5-9630-488b-8a68-b6c95b49b422"
     },
     {
         "id" : "3",
         "name" : "test3",
-        "email" : "testuser9abc@kosen.com",
+        "email" : "testuser3456@kosen.com",
         "password" : "testuser3456",
         "token" : "10868347-43bc-4dd4-872a-bb69f9442dd3"
     }
