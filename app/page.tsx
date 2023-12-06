09b93f709e5977724a1af5ee572da29733add74f
se4weed <norisama1159@gmail.com>
2023-12-07 02:35:11 +0900

    ログイン状態にリダイレクト先を修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,24 @@
 
 import { Loading } from "@/components/Loading"
 import { useRouter } from "next/navigation"
+import { parseCookies } from "nookies";
 import { useEffect } from "react"
 
 export default function Page() {
-  const router = useRouter()
-  useEffect (() => {
-    router.push("/login");
-  })
+  const router = useRouter();
+  useEffect(() => {
+    const cookies = parseCookies();
+    const checkCookie = async () => {
+      if (cookies.token){
+        // ログイン状態
+        router.push("/event");
+      }else{
+        // ログインしていない状態
+        router.push("/login");
+      }
+    }
+    checkCookie();
+  }, []);
   return (
     <Loading />
   )
