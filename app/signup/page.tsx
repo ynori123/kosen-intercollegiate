fc779e55ad2f54f26664983f555aafd019414a2c
se4weed <norisama1159@gmail.com>
2023-12-06 20:12:03 +0900

    アカウント作成ページの変更

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 import Icon from "public/icon.svg"
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function page() {
+  const [demoAlert, setDemoAlert] = useState(false);
+
+  const handleSubmit = () => {
+    setDemoAlert(true);
+  }
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
@@ -11,10 +16,11 @@ export default function page() {
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               アカウント作成
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <div className="space-y-4 md:space-y-6">
+              {demoAlert ? <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">デモのためアカウントの登録はできません．デモアカウントを使用してください．</div> : ""}
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">メールアドレス</label>
                 <input
@@ -66,6 +72,7 @@ export default function page() {
               </div>
               <button
                 type="submit"
+                onClick={handleSubmit}
                 className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
               >
                 アカウント作成
@@ -73,7 +80,7 @@ export default function page() {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 すでにアカウントを持っていますか？ <a href="/login" className="font-medium text-slate-600 hover:underline dark:text-slate-500">Login here</a>
               </p>
-            </form>
+            </div>
           </div>
         </div>
       </div>
