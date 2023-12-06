d74a5ce5f3143aa7c42588ab5e79bb8f744a635a
se4weed <norisama1159@gmail.com>
2023-12-06 20:09:32 +0900

    ログインにリダイレクトされるように

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
-import Link from "next/link"
+import { Loading } from "@/components/Loading"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Page() {
+  const router = useRouter()
+  useEffect (() => {
+    router.push("/login");
+  })
   return (
-    <>
-      index page
-    </>
+    <Loading />
   )
 }
