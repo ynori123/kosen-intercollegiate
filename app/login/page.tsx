0ac9b2a9671127ee660368871c242ba0f52f2f38
se4weed <norisama1159@gmail.com>
2023-12-07 02:38:26 +0900

    ログインを実装

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,66 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { parseCookies, setCookie } from 'nookies';
 import Icon from "public/icon.svg";
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function page() {
+  const router = useRouter();
+  useEffect(() => {
+    const cookies = parseCookies();
+    const checkCookie = async () => {
+      if (cookies.token){
+        router.push("/");
+      }
+    }
+    checkCookie();
+  }, []);
+  interface FormData {
+    email: string;
+    password: string;
+  }
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      email: e.target.value,
+    }));
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      password: e.target.value,
+    }));
+  };
+  const handleSubmit = () => {
+    // process.env.NEXT_PUBLIC_BACK_URL
+    console.log(formData);
+    const postData = async () => {
+      try{
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_URL}/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data: {code: number, token: string} = await response.json() || {code: 0, token: ""};
+        if (data?.code === 0){
+          setCookie(null, "token", data?.token, {maxAge: 30*24*60*60});
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    postData();
+  };
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
@@ -14,13 +72,15 @@ export default function page() {
             <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               ログイン
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <div className="space-y-4 md:space-y-6">
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">メールアドレス</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
+                  value={formData.email}
+                  onChange={handleChangeEmail}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required
@@ -32,6 +92,8 @@ export default function page() {
                   type="password"
                   name="password"
                   id="password"
+                  value={formData.password}
+                  onChange={handleChangePassword}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
@@ -39,6 +101,7 @@ export default function page() {
               </div>
               <button
                 type="submit"
+                onClick={handleSubmit}
                 className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
               >
                 ログイン
@@ -46,7 +109,7 @@ export default function page() {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 アカウントを持っていませんか？ <a href="/login" className="font-medium text-slate-600 hover:underline dark:text-slate-500">Login here</a>
               </p>
-            </form>
+            </div>
           </div>
         </div>
       </div>
