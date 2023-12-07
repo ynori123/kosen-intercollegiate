d038ae404ae24141d00788c32f59c66120ae4253
ynori123 <norisama1159@gmail.com>
2023-12-07 14:13:28 +0900

    disable dark mode

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -62,32 +62,32 @@ export default function Page() {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
-        <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
           <Icon className="w-10 h-10"/>
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               ログイン
             </h1>
             <div className="space-y-4 md:space-y-6">
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">メールアドレス</label>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
                   value={formData.email}
                   onChange={handleChangeEmail}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   required
                 />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">パスワード</label>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">パスワード</label>
                 <input
                   type="password"
                   name="password"
@@ -95,19 +95,19 @@ export default function Page() {
                   value={formData.password}
                   onChange={handleChangePassword}
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5"
                   required
                 />
               </div>
               <button
                 type="submit"
                 onClick={handleSubmit}
-                className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
+                className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 ログイン
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                アカウントを持っていませんか？ <a href="/login" className="font-medium text-slate-600 hover:underline dark:text-slate-500">Login here</a>
+              <p className="text-sm font-light text-gray-500">
+                アカウントを持っていませんか？ <a href="/login" className="font-medium text-slate-600 hover:underline ">Login here</a>
               </p>
             </div>
           </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,20 +10,20 @@ export default function Page() {
     setDemoAlert(true);
   }
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
-        <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
           <Icon className="w-10 h-10"/>
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               アカウント作成
             </h1>
             <div className="space-y-4 md:space-y-6">
               {demoAlert ? <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">デモのためアカウントの登録はできません．デモアカウントを使用してください．</div> : ""}
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">メールアドレス</label>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
                 <input
                   id="email"
                   name="email"
@@ -62,12 +62,12 @@ export default function Page() {
               <button
                 type="submit"
                 onClick={handleSubmit}
-                className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
+                className="w-full text-white bg-slate-600 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 アカウント登録
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                すでにアカウントを持っていますか？ <Link href="/login" className="font-medium text-slate-600 hover:underline dark:text-slate-500">Login here</Link>
+              <p className="text-sm font-light text-gray-500">
+                すでにアカウントを持っていますか？ <Link href="/login" className="font-medium text-slate-600 hover:underline">Login here</Link>
               </p>
             </div>
           </div>
