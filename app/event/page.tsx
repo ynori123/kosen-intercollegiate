64a4a543a06771aa692141a1eb8f49e19eb7940e
ynori123 <norisama1159@gmail.com>
2023-12-07 14:44:54 +0900

    URLを修正

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     if (effectRan.current === false){
       const fetchData = async () => {
         try {
-          const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_URL}/offers`);
+          const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/offers`);
           const data = await response.json();
           
           const offers: [{id: string, title: string, place: string, datetime: string, imgSrc: string}] = data?.data?.offers || [];
